2e7bf31600876b6ac927f4c8007b66af5089e1a5
czajkowskis <szymonczajkowski022@gmail.com>
2024-12-10 23:28:08 +0100

    Added skeletons for quiz view and home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script>
+  import { RouterLink } from 'vue-router'
+  
+  export default {
+    data() {
+      return {
+        words: null
+      };
+    },
+    mounted() {
+      // Fetch JSON from the public directory
+      fetch('/data/nouns.json')
+        .then(response => response.json())
+        .then(data => {
+          this.words = data;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+    }
+  };
+</script>
+
+<template>
+  <div>
+    <h1>Home View</h1>
+    <pre>{{ jsonData }}</pre>
+    <RouterLink :to="{name: 'Learn'}">Learn</RouterLink>
+    <RouterLink :to="{name: 'Quiz'}">Quiz</RouterLink>
+  </div>
+</template>
+
---
+++ src/views/QuizView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Quiz View</h1>
+  </div>
+  <RouterLink :to="{name: 'Learn'}">Learn</RouterLink>
+  <RouterLink :to="{name: 'Quiz', params: {quizDeck: nouns_questions}}">Quiz</RouterLink>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      words: null
+    };
+  },
+  mounted() {
+    // Fetch JSON from the public directory
+    fetch('/data/nouns.json')
+      .then(response => response.json())
+      .then(data => {
+        this.words = data;
+      })
+      .catch(error => console.error('Error fetching JSON:', error));
+  }
+};
+</script>
